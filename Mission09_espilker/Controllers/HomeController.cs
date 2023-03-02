93e5686898ea7656bcad8ec55ddcfcba2225d6bd
espilker <ethanspilker@gmail.com>
2023-03-01 19:10:14 -0700

    Add project files.

---
+++ Mission09_espilker/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_espilker.Models;
+using Mission09_espilker.Models.View_Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_espilker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                    .OrderBy(b => b.Title)
+                    .Skip((pageNum - 1) * pageSize) // .Take(5) Only shows five results .Skip(5) would skip 5 entries
+                    .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+    }
+}
